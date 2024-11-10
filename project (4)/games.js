00b683289a94d6ef367684fc58574e9cf9e1d8c0
whynames <b.s_fb@hotmail.com>
2024-11-10 03:30:17 +0300

    first push

---
+++ project (4)/games.js
@@ -0,0 +1,24 @@
+const games = [
+  {
+    id: "0d95d64a4b8e440981a7b067c483cdf2",
+    title: "Tile Match Connect 3 Tiles",
+    description: "Match and connect tiles in this exciting puzzle game",
+    category: "Puzzle",
+    thumbnail: "https://img.gamedistribution.com/0d95d64a4b8e440981a7b067c483cdf2-512x512.jpg",
+    rating: 4.5,
+    type: "embedded"
+  },
+  {
+    id: "unity-webgl-game",
+    title: "Unity WebGL Game",
+    description: "An exciting Unity WebGL game",
+    category: "Action",
+    thumbnail: "path/to/unity-game-thumbnail.jpg",
+    rating: 4.8,
+    type: "unity",
+    path: "/games/unity-webgl-game/index.html"
+  }
+  // ... other games ...
+];
+
+console.log('Games loaded in games.js:', games.length);
---
+++ project (4)/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
+
+// Serve Unity game files
+app.use('/games', express.static(path.join(__dirname, 'games')));
+
+// Handle all routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
